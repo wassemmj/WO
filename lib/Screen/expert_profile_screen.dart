4b44b655e73a4d3e4ae40f564300aebb6532bbe1
wassemmj <wassemmjarkesh03@gmail.com>
2022-12-08 15:36:12 +0300

    kd

--- lib/Screen/expert_profile_screen.dart
+++ lib/Screen/expert_profile_screen.dart
@@ -19,12 +19,18 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
 
   String _startTime = DateFormat('hh:mm a').format(DateTime.now()).toString();
   String _endTime = DateFormat('hh:mm a')
-      .format(DateTime.now().add(const Duration(minutes: 15)))
+      .format(DateTime.now().add(const Duration(minutes: 120)))
       .toString();
 
   var addressController = TextEditingController();
   var phoneController = TextEditingController();
   var skillController = TextEditingController();
+
+  bool _isC1 = false;
+  bool _isC2 = false;
+  bool _isC3 = false;
+  bool _isC4 = false;
+  bool _isC5 = false;
 
   @override
   Widget build(BuildContext context) {
@@ -32,25 +38,25 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            height: MediaQuery.of(context).size.height,
             decoration: const BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Colors.purple, Colors.white,],
-                  begin: Alignment.topLeft,
-                  end: Alignment.center,
-                )),
+              colors: [
+                Colors.purple,
+                Colors.white,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.center,
+            )),
             padding: const EdgeInsets.all(25),
             child: Form(
               key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    //padding: const EdgeInsets.only(bottom: 100),
-                    child: Text(
-                      'Name,Complete Your Profile',
-                      style: GoogleFonts.bebasNeue(fontSize: 35, color: Colors.white),
-                    ),
+                  Text(
+                    'Name,Complete Your Profile',
+                    style: GoogleFonts.bebasNeue(
+                        fontSize: 35, color: Colors.white),
                   ),
                   const SizedBox(height: 10),
                   Padding(
@@ -60,12 +66,14 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
                       children: [
                         Container(
                           padding: const EdgeInsets.all(8),
-                          decoration: const BoxDecoration(color: Colors.white70, shape: BoxShape.circle),
+                          decoration: const BoxDecoration(
+                              color: Colors.white70, shape: BoxShape.circle),
                           child: ClipOval(
                             child: SizedBox.fromSize(
                               size: const Size.fromRadius(50),
                               child: pickedImage == null
-                                  ? Image.asset('images/person.jpeg',fit: BoxFit.fitHeight)
+                                  ? Image.asset('images/person.jpeg',
+                                      fit: BoxFit.fitHeight)
                                   : Image.file(pickedImage!),
                             ),
                           ),
@@ -73,8 +81,8 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
                         const SizedBox(height: 10),
                         ElevatedButton.icon(
                           onPressed: () async {
-                            final XFile? image =
-                            await picker.pickImage(source: ImageSource.gallery);
+                            final XFile? image = await picker.pickImage(
+                                source: ImageSource.gallery);
                             if (image == null) {
                               return;
                             }
@@ -84,7 +92,7 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
                           },
                           style: ButtonStyle(
                               backgroundColor:
-                              MaterialStateProperty.all(Colors.purple)),
+                                  MaterialStateProperty.all(Colors.purple)),
                           icon: const Icon(Icons.image),
                           label: const Text('Image'),
                         )
@@ -116,12 +124,10 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
                     controller: phoneController,
                     keyboardType: TextInputType.number,
                     validator: (val) {
-                      final bool phoneValid =
-                      RegExp(r"^[0-9.]")
-                          .hasMatch(val!);
-                      if(val.isEmpty) {
+                      final bool phoneValid = RegExp(r"^[0-9.]").hasMatch(val!);
+                      if (val.isEmpty) {
                         return 'phone Number is Required';
-                      } else if(!phoneValid) {
+                      } else if (!phoneValid || val.length < 10) {
                         return 'Not a Valid Phone Number';
                       }
                     },
@@ -151,7 +157,7 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
                     cursorColor: Colors.purple,
                     controller: addressController,
                     validator: (val) {
-                      if(val!.isEmpty) {
+                      if (val!.isEmpty) {
                         return 'Address is Required';
                       }
                     },
@@ -180,9 +186,9 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
                     ),
                     cursorColor: Colors.purple,
                     controller: skillController,
-                    maxLines: 4,
+                    maxLines: 2,
                     validator: (val) {
-                      if(val!.isEmpty) {
+                      if (val!.isEmpty) {
                         return 'Skill is Required';
                       }
                     },
@@ -204,67 +210,149 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
                   Row(
                     children: [
                       Expanded(
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Start Time',
-                            labelStyle: const TextStyle(
-                              color: Colors.purple,
-                              fontSize: 20,
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.purple),
-                              borderRadius: BorderRadius.circular(25),
-                            ),
-                            focusColor: Colors.purple,
-                            focusedBorder: const UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.purple),
-                            ),
-                              suffixIcon: IconButton(
-                                onPressed: () =>
-                                    _getTimeFromUser(isStart: true),
-                                icon: const Icon(
-                                  Icons.access_time_rounded,
-                                  color: Colors.grey,
-                                ),
-                              )
+                        child: TextButton.icon(
+                          onPressed: () => _getTimeFromUser(isStart: true),
+                          icon: const Icon(
+                            Icons.access_time_rounded,
+                            color: Colors.purple,
                           ),
-                          cursorColor: Colors.purple,
-                          style: const TextStyle(color: Colors.black54),
+                          label: Text(
+                            _startTime,
+                            style: const TextStyle(color: Colors.purple),
+                          ),
+                        ),
+                      ),
+                      const Text(
+                        'to',
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 18,
                         ),
                       ),
                       Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 25),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              border: Border.all(color: Colors.white),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 20),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: _endTime,
-                                  hintStyle: const TextStyle(color: Colors.black),
-                                  suffixIcon: IconButton(
-                                    onPressed: () =>
-                                        _getTimeFromUser(isStart: false),
-                                    icon: const Icon(
-                                      Icons.access_time_rounded,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ),
-                                cursorColor: Colors.purple,
-                                style: const TextStyle(color: Colors.black),
-                              ),
-                            ),
+                        child: TextButton.icon(
+                          onPressed: () => _getTimeFromUser(isStart: false),
+                          icon: const Icon(
+                            Icons.access_time_rounded,
+                            color: Colors.purple,
+                          ),
+                          label: Text(
+                            _endTime,
+                            style: const TextStyle(color: Colors.purple),
                           ),
                         ),
                       ),
                     ],
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                      ),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Column(
+                      children: [
+                        CheckboxListTile(
+                          title: const Text('c1',style: TextStyle(color:Colors.purple,fontSize: 20),),
+                          value: _isC1,
+                          activeColor: Colors.purple,
+                          checkColor: Colors.white,
+                          onChanged: (val) => setState(() {
+                            _isC1 = val!;
+                          }),
+                        ),
+                        CheckboxListTile(
+                          title: const Text('c2',style: TextStyle(color:Colors.purple,fontSize: 20),),
+                          value: _isC2,
+                          activeColor: Colors.purple,
+                          checkColor: Colors.white,
+                          onChanged: (val) => setState(() {
+                            _isC2 = val!;
+                          }),
+                        ),
+                        CheckboxListTile(
+                          title: const Text('c3',style: TextStyle(color:Colors.purple,fontSize: 20),),
+                          value: _isC3,
+                          activeColor: Colors.purple,
+                          checkColor: Colors.white,
+                          onChanged: (val) => setState(() {
+                            _isC3 = val!;
+                          }),
+                        ),
+                        CheckboxListTile(
+                          title: const Text('c4',style: TextStyle(color:Colors.purple,fontSize: 20),),
+                          value: _isC4,
+                          activeColor: Colors.purple,
+                          checkColor: Colors.white,
+                          onChanged: (val) => setState(() {
+                            _isC4 = val!;
+                          }),
+                        ),
+                        CheckboxListTile(
+                          title: const Text('c5',style: TextStyle(color:Colors.purple,fontSize: 20),),
+                          value: _isC5,
+                          activeColor: Colors.purple,
+                          checkColor: Colors.white,
+                          onChanged: (val) => setState(() {
+                            _isC5 = val!;
+                          }),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  InkWell(
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Colors.purple,
+                            Colors.white70,
+                            Colors.purple,
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Done',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                            "Register Successfully",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          duration: Duration(seconds: 1),
+                        ));
+                        //Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> const CatScreen()));
+                        _formKey.currentState!.save();
+                        phoneController.clear();
+                        addressController.clear();
+                        skillController.clear();
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                            "Try Again!",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          duration: Duration(seconds: 1),
+                        ));
+                      }
+                    },
                   ),
                 ],
               ),
@@ -282,7 +370,7 @@ class _ExpertProfileScreenState extends State<ExpertProfileScreen> {
       initialTime: isStart
           ? TimeOfDay.fromDateTime(DateTime.now())
           : TimeOfDay.fromDateTime(
-          DateTime.now().add(const Duration(minutes: 15))),
+              DateTime.now().add(const Duration(minutes: 15))),
     );
     String formattedTime = pickedTime!.format(context);
     if (isStart) {
