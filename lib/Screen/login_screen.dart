94357418111f42ef3d424214101b202a932e5ae6
wassemmj <wassemmjarkesh03@gmail.com>
2022-12-08 12:32:32 +0300

    -

--- lib/Screen/login_screen.dart
+++ lib/Screen/login_screen.dart
@@ -17,178 +17,183 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          colors: [Colors.purple, Colors.white],
-          begin: Alignment.topLeft,
-          end: Alignment.center,
-        )),
-        padding: const EdgeInsets.all(25),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 100),
-                child: Text(
-                  'Log In',
-                  style: GoogleFonts.bebasNeue(fontSize: 52, color: Colors.white),
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Enter Email',
-                  hintStyle: const TextStyle(
-                    color: Colors.black54,
-                  ),
-                  labelText: 'Email',
-                  labelStyle: const TextStyle(
-                    color: Colors.purple,
-                    fontSize: 20,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.purple),
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  focusColor: Colors.purple,
-                  focusedBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
-                  ),
-                ),
-                cursorColor: Colors.purple,
-                controller: emailController,
-                validator: (val) {
-                  final bool emailValid =
-                  RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                      .hasMatch(val!);
-                  if(val.isEmpty) {
-                    return 'Email is Required';
-                  } else if(!emailValid) {
-                    return 'Not a Valid Email';
-                  }
-                },
-                style: const TextStyle(color: Colors.black54),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Enter Password',
-                  hintStyle: const TextStyle(
-                    color: Colors.black54,
-                  ),
-                  labelText: 'Password',
-                  labelStyle: const TextStyle(
-                    color: Colors.purple,
-                    fontSize: 20,
-                  ),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      obscure ? Icons.visibility : Icons.visibility_off,
-                      color: Colors.purple,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        obscure = !obscure;
-                      });
-                    },
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.purple),
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  focusColor: Colors.purple,
-                  focusedBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.purple),
-                  ),
-                ),
-                cursorColor: Colors.purple,
-                obscureText: obscure,
-                controller: passwordController,
-                validator: (val) {
-                  if(val!.isEmpty) {
-                    return 'Password is Required';
-                  } else if(val.length<8) {
-                    return 'Short Password';
-                  }
-                },
-                style: const TextStyle(color: Colors.black54),
-              ),
-              const SizedBox(height: 20),
-              InkWell(
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [
-                        Colors.purple,
-                        Colors.white70,
-                        Colors.purple,
-                      ],
-                    ),
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: const Center(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.purple, Colors.white,],
+                  begin: Alignment.topLeft,
+                  end: Alignment.center,
+                )),
+            //height: 1000,
+            padding: const EdgeInsets.all(25),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 100),
                     child: Text(
                       'Log In',
-                      style: TextStyle(
+                      style: GoogleFonts.bebasNeue(fontSize: 52, color: Colors.white),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Email',
+                      hintStyle: const TextStyle(
                         color: Colors.black54,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
                       ),
-                    ),
-                  ),
-                ),
-                onTap: () {
-                  if(_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Log in Successfully",style: TextStyle(fontSize: 15),),
-                          duration: Duration(seconds: 1),
-                        )
-                    );
-                    //Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> const CatScreen()));
-                    _formKey.currentState!.save();
-                    emailController.clear();
-                    passwordController.clear();
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Try Again!",style: TextStyle(fontSize: 15),),
-                          duration: Duration(seconds: 1),
-                        )
-                    );
-                  }
-                },
-              ),
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black54,
-                    ),
-                  ),
-                  InkWell(
-                    child: Text(
-                      'Register Now',
-                      style: TextStyle(
+                      labelText: 'Email',
+                      labelStyle: const TextStyle(
                         color: Colors.purple,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.purple),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      focusColor: Colors.purple,
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
                       ),
                     ),
-                    // onTap: () {
-                    //   Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    //       builder: (_) => widget.user? const SingUpScreen():const SingUpExpert()));
-                    // },
+                    cursorColor: Colors.purple,
+                    controller: emailController,
+                    validator: (val) {
+                      final bool emailValid =
+                      RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(val!);
+                      if(val.isEmpty) {
+                        return 'Email is Required';
+                      } else if(!emailValid) {
+                        return 'Not a Valid Email';
+                      }
+                    },
+                    style: const TextStyle(color: Colors.black54),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Password',
+                      hintStyle: const TextStyle(
+                        color: Colors.black54,
+                      ),
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(
+                        color: Colors.purple,
+                        fontSize: 20,
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          obscure ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.purple,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            obscure = !obscure;
+                          });
+                        },
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.purple),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      focusColor: Colors.purple,
+                      focusedBorder: const UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.purple),
+                      ),
+                    ),
+                    cursorColor: Colors.purple,
+                    obscureText: obscure,
+                    controller: passwordController,
+                    validator: (val) {
+                      if(val!.isEmpty) {
+                        return 'Password is Required';
+                      } else if(val.length<8) {
+                        return 'Short Password';
+                      }
+                    },
+                    style: const TextStyle(color: Colors.black54),
+                  ),
+                  const SizedBox(height: 20),
+                  InkWell(
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Colors.purple,
+                            Colors.white,
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      if(_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Log in Successfully",style: TextStyle(fontSize: 15),),
+                              duration: Duration(seconds: 1),
+                            )
+                        );
+                        //Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> const CatScreen()));
+                        _formKey.currentState!.save();
+                        emailController.clear();
+                        passwordController.clear();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Try Again!",style: TextStyle(fontSize: 15),),
+                              duration: Duration(seconds: 1),
+                            )
+                        );
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Not a member?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      InkWell(
+                        child: Text(
+                          'Register Now',
+                          style: TextStyle(
+                            color: Colors.purple,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        // onTap: () {
+                        //   Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        //       builder: (_) => widget.user? const SingUpScreen():const SingUpExpert()));
+                        // },
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
